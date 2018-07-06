a4c99e42fbc4b63ba9c9f993de4c436417a84591
PennController <39499242+PennController@users.noreply.github.com>
2018-07-06 10:21:19 +0200

    Add files via upload

---
+++ data_includes/example_data.js
@@ -0,0 +1,14 @@
+var shuffleSequence = seq("trial");
+PennController.ResetPrefix(null);
+
+var items = [
+    ["trial", "PennController", PennController(
+        newTextInput("alternative")
+            .settings.before("I thought the weather was warm, but it really is ")
+            .print()
+        ,
+        newButton("validation", "Click here to continue.")
+            .print()
+            .wait()
+    )]
+];
